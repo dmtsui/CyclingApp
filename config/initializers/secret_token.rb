9dfdc9d3d4838fc63fdf94e958a59b48ec590157
Dan Tsui <dmtsui@gmail.com>
2013-04-26 10:57:21 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CyclingApp::Application.config.secret_token = 'ba96d1660659d4e7e6bd6e3ba2c5ae1583d9d3d177ad1c6c50b90fd095544173461f4a5197ee2a97b474b5529210589618046c93410e5b4b829bf07094469a5f'
